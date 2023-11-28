27f02e0b8db976df06460707ca802ce9ca627ed8
Futozhka <124589958+Futozhka@users.noreply.github.com>
2023-11-28 13:20:40 +0000

    start2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+
+  let intervalId = null;
+
+  document.getElementById('startButton').addEventListener('click', function() {
+    
+    const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
+
+    
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+
+    intervalId = setInterval(function() {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+
+  document.getElementById('stopButton').addEventListener('click', function() {
+
+    const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
+
+    
+    startButton.disabled = false;
+    stopButton.disabled = true;
+
+  
+    clearInterval(intervalId);
+  });
